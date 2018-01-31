33977f4ad74de41b73429e116492279c7dffdb65
Leonardo Santos <leocs.1991@gmail.com>
2018-01-31 18:35:51 -0200

    Updated rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -5,13 +5,15 @@ const pkg    = require('./package.json');
 const banner = `/*! ${pkg.title} v${pkg.version} (${new Date().toString().substr(4, 11)}) - ${pkg.homepage} - Copyright (c) ${new Date().getFullYear()} Leonardo Santos; MIT License */`;
 
 export default {
-  entry: 'src/menuspy.js',
-  dest: 'dist/menuspy.js',
-  format: 'umd',
-  moduleName: 'MenuSpy',
+  input: 'src/menuspy.js',
+  output: {
+    name: 'MenuSpy',
+    file: 'dist/menuspy.js',
+    format: 'umd',
+    banner: banner
+  },
   plugins: [
     eslint(),
     buble()
-  ],
-  banner: banner
+  ]
 };
